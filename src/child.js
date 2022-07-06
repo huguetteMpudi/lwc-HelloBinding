27374ac6e00258b8b342667baa74dbd6f97edae9
Huguette Mpudi <41489109+undefined@users.noreply.github.com>
2022-07-06 05:58:45 +0000

    Push from lwc_recipes

--- src/child.js
+++ src/child.js
@@ -2,8 +2,23 @@ import { LightningElement } from 'lwc';
 
 export default class HelloBinding extends LightningElement {
     greeting = 'World';
+    firstName = '';
+    lastName = '';
+
+    handleChangeA(event) {
+        this.greeting = event.target.value;
+    }
 
     handleChange(event) {
-        this.greeting = event.target.value;
+        const field = event.target.name;
+        if (field === 'firstName') {
+            this.firstName = event.target.value;
+        } else if (field === 'lastName') {
+            this.lastName = event.target.value;
+        }
+    }
+
+    get uppercasedFullName() {
+        return `${this.firstName} ${this.lastName}`.toUpperCase();
     }
 }
