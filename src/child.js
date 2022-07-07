b22e442743d24082b8bc4a57647ddae57b34e3af
Huguette Mpudi <41489109+undefined@users.noreply.github.com>
2022-07-07 01:01:38 +0000

    Push from lwc_recipes

--- src/child.js
+++ src/child.js
@@ -1,6 +1,20 @@
 import { LightningElement } from 'lwc';
+import templateOne from './templateOne.html';
+import templateTwo from './templateTwo.html';
 
 export default class HelloBinding extends LightningElement {
+
+templateOne = true;
+
+    render() {
+       return this.templateOne ? templateOne : templateTwo;
+}
+
+    switchTemplate(){ 
+        this.templateOne = this.templateOne === true ? false : true; 
+    }
+
+
 
     contacts = [
         {
